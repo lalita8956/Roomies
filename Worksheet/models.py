2b30a96e54b28625b6033aacc8f2b914048c556b
lalita <lalitamali6789@gmail.com>
2025-08-06 11:37:29 +0530

    start app

---
+++ Worksheet/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+
+class Work(models.Model):
+    Work_name = models.TextField()
+    day = models.CharField(default=None, max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.Work_name
+ 
+class Task(models.Model):
+    task_name = models.TextField()
+    work = models.ForeignKey(Work, on_delete=models.CASCADE, related_name='tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.task_name
+
+
