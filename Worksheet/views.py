4b2629ecac1b3b9fc27efcd35d72965fb4bc4803
lalita <lalitamali6789@gmail.com>
2025-08-06 16:36:57 +0530

    admin

--- Worksheet/views.py
+++ Worksheet/views.py
@@ -2,7 +2,7 @@
 from .models import Work
 from django.views.generic import ListView
 
-class TodoListView(ListView):
+class WorkListView(ListView):
     model = Work
     template_name = "app/index.html"
     context_object_name= 'work'
